2e347cff8811025701cbcb2f8f6c055158f8d3fc
andersonkaiti <anderkaiti@gmail.com>
2026-02-05 23:46:51 -0300

    :truck: refactor(project): rename project to react-native-bare-course using react-native-rename

--- ios/reactnativebarecourse/AppDelegate.swift
+++ ios/reactnativebarecourse/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MyApp",
+      withModuleName: "reactnativebarecourse",
       in: window,
       launchOptions: launchOptions
     )

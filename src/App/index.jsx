7e81a865d8cd9d36e8e907a4bfb8a942bb19c09b
andersonkaiti <anderkaiti@gmail.com>
2026-02-10 23:06:17 -0300

    :sparkles: feat(touchable-highlight): introduce TouchableHighlight component and its props

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,6 +3,7 @@ import {
   SafeAreaView,
   ScrollView,
   Text,
+  TouchableHighlight,
   TouchableOpacity,
   View,
 } from 'react-native'
@@ -19,27 +20,39 @@ export function App() {
             onPress={() => alert('Button pressed')}
           />
 
-          {/*
-            Os elementos Touchable permitem que qualquer elemento da ui,
-            como uma View, uma imagem, etc., seja pressionável.
-
-            O "Opacity" do TouchableOpacity está relacionado com o tipo de
-            feedback quando o componente é pressionado, ou seja, tudo dentro
-            dele escurece quando é pressionado.
-
-            A prop "activeOpacity" define o nível de opacidade quando o
-            componente é pressionado.
-
-            Ele aceita a prop style.
-          */}
-
           <TouchableOpacity
             activeOpacity={0.5}
             onPress={() => alert('TouchableOpacity pressed')}
             style={styles.button}
           >
             <Text style={styles.buttonLabel}>TouchableOpacity</Text>
+            <Text style={styles.buttonLabel}>TouchableOpacity</Text>
           </TouchableOpacity>
+
+          {/* 
+            Quando o TouchableHighlight é pressionado, ele recebe um destaque,
+            ou seja, uma cor de fundo é aplicada nele, que por padrão é preta,
+            mas pode ser alterada usando a prop underlayColor.
+
+            O highlight só funciona caso ele tenha um onPress.
+
+            Além disso, ao ser pressionado, o texto dentro dele tem a opacidade
+            alterada, mas isso pode ser alterado usando a prop activeOpacity,
+            que transfere o valor para o filho.
+
+            Limitação: O TouchableHighlight não aceita múltiplos filhos.
+          */}
+          <TouchableHighlight
+            onPress={() => alert('TouchableHighlight pressed')}
+            style={styles.button}
+            underlayColor="#f00"
+            activeOpacity={0.5}
+          >
+            <View>
+              <Text style={styles.buttonLabel}>TouchableHighlight</Text>
+              <Text style={styles.buttonLabel}>TouchableHighlight</Text>
+            </View>
+          </TouchableHighlight>
         </View>
       </ScrollView>
     </SafeAreaView>

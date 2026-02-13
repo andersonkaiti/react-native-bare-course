0fc9554dac47e263da113bbac1cc2c65658632cc
andersonkaiti <anderkaiti@gmail.com>
2026-02-12 22:23:09 -0300

    :sparkles: feat(text-input): introduce some TextInput props

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,35 +2,22 @@ import { SafeAreaView, TextInput, View } from 'react-native'
 import { Button } from '../components/Button'
 import { styles } from './styles'
 
-// Enquanto na web, para criarmos os formulários, utilizamos o input, que
-// contém várias aparências e funcionalidades, no React Native temos apenas o
-// TextInput como componente nativo, que apenas aceita texto. Qualquer outro
-// tipo de input como Checkbox, RadioButton, etc., deve ser criado do zero.
-
 export function App() {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        {/* 
-          Tanto no Android como no iOS, o TextInput não vem com nenhum estilo
-          pré-definido.
-
-          No iOS, a altura do input é a mesma do caret (barrinha que fica
-          piscando ao digitar), então é recomendável definir uma altura mínima.
-
-          A tipagem da prop style do TextInput é a mesma da prop style do
-          componente Text: style?: StyleProp<TextStyle> | undefined, então é
-          possível definir a cor do texto do input, por exemplo.
-
-          O placeholder é um texto que aparece quando o input está vazio. A sua
-          cor é definida pela prop placeholderTextColor.
-
-          Para esconder a entrada de texto, usamos a prop secureTextEntry.
-        */}
         <TextInput
           style={styles.input}
           placeholder="E-mail"
           placeholderTextColor="#aaa"
+          editable
+          // readOnly
+          // textAlign="left"
+          // maxLength={9}
+          // caretHidden={false}
+          // cursorColor="#f00" // * Android only
+          // selectionColor="#f00" // Também altera o cursor do iOS
+          autoFocus // Foca o input ao abrir a tela
         />
 
         <TextInput

ed984dcb81dc3d5ecbd03baaebd1899207e8d267
andersonkaiti <anderkaiti@gmail.com>
2026-02-13 23:49:13 -0300

    :sparkles: feat(input): convert inputs to controlled components and capture their values

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { SafeAreaView, View } from 'react-native'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
@@ -6,6 +6,16 @@ import { styles } from './styles'
 
 export function App() {
   const passwordInputRef = useRef(null)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit() {
+    console.log({
+      email,
+      password,
+    })
+  }
 
   return (
     <SafeAreaView style={styles.wrapper}>
@@ -16,8 +26,22 @@ export function App() {
           autoCapitalize="none"
           autoCorrect={false}
           autoComplete="email"
-          onSubmitEditing={() => passwordInputRef.current.focus()}
+          onSubmitEditing={() => {
+            passwordInputRef.current.focus()
+          }}
           returnKeyType="next"
+          // Mesmo que o componente passe a ser controlled ao adicionar a prop
+          // value e o onChange não esteja definido, o valor aparece e some
+          // pois isso é controlado do lado nativo. Então o lado nativo informa
+          // essa alteração para o JavaScript, mas como o valor no JavaScript
+          // não foi alterado, ele retorna ao estado inicial.
+          value={email}
+          // Neste caso, o event contém todas as informações.
+          // onChange={(event) => {
+          //   setEmail(event.nativeEvent.text)
+          // }}
+          // E neste, o que está disponível é o texto do input.
+          onChangeText={setEmail}
         />
 
         <Input
@@ -26,10 +50,12 @@ export function App() {
           secureTextEntry
           keyboardType="number-pad"
           returnKeyType="done"
-          onSubmitEditing={() => console.log('send')}
+          onSubmitEditing={handleSubmit}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <Button>Enviar</Button>
+        <Button onPress={handleSubmit}>Enviar</Button>
       </View>
     </SafeAreaView>
   )
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,11 +3,6 @@ import { TextInput } from 'react-native'
 import { styles } from './styles'
 
 /**
- * import("react").FunctionComponent | import("react").FC -> React.FC
- * Define a variável Input como sendo do tipo Functional Component
- * contendo as props do TextInput. É melhor utilizar o React, porque ele é
- * global, então não é necessário importar nada.
- *
  * @type {React.FC<import("react-native").TextInputProps>}
  */
 export const Input = forwardRef((props, ref) => {

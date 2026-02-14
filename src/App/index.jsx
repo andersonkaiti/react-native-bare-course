b4f713a24296f92e4c5228bac4fd0d57370b8d47
andersonkaiti <anderkaiti@gmail.com>
2026-02-13 23:09:45 -0300

    :sparkles: feat(input): configure keyboard return key behavior

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -9,18 +9,30 @@ export function App() {
       <View style={styles.container}>
         <Input
           placeholder="E-mail"
-          // Configura que tipo de teclado deve ser exibido
           keyboardType="email-address"
-          // Configura que não deve capitalizar a primeira letra
           autoCapitalize="none"
-          // Configura que não deve corrigir a primeira letra
           autoCorrect={false}
           autoComplete="email"
+          // O onSubmitEditing é chamado quando o usuário pressiona a tecla
+          // "Enter" no teclado
+          onSubmitEditing={() => console.log('clicked')}
+          // Mantém o teclado aberto após o submit
+          submitBehavior="submit"
+          returnKeyType="next"
+          // Por padrão é false e impede a execução do onSubmitEditing caso o
+          // campo esteja vazio
+          enablesReturnKeyAutomatically // * iOS only
         />
 
-        <Input placeholder="Senha" secureTextEntry keyboardType="number-pad" />
+        <Input
+          placeholder="Senha"
+          secureTextEntry
+          keyboardType="number-pad"
+          returnKeyType="done"
+          onSubmitEditing={() => console.log('send')}
+        />
 
-        <Button>Hello, Button!</Button>
+        <Button>Enviar</Button>
       </View>
     </SafeAreaView>
   )

193a9eb925e2b81b6cc442bd87538678dcc2485f
andersonkaiti <anderkaiti@gmail.com>
2026-02-13 21:57:02 -0300

    :sparkles: feat(input): introduce new TextInput props to enhance typing UX

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -7,9 +7,18 @@ export function App() {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Input placeholder="E-mail" />
+        <Input
+          placeholder="E-mail"
+          // Configura que tipo de teclado deve ser exibido
+          keyboardType="email-address"
+          // Configura que não deve capitalizar a primeira letra
+          autoCapitalize="none"
+          // Configura que não deve corrigir a primeira letra
+          autoCorrect={false}
+          autoComplete="email"
+        />
 
-        <Input placeholder="Senha" secureTextEntry />
+        <Input placeholder="Senha" secureTextEntry keyboardType="number-pad" />
 
         <Button>Hello, Button!</Button>
       </View>

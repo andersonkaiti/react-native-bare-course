1b24db4c90b26793da6ee043a9668470cdee57d2
andersonkaiti <anderkaiti@gmail.com>
2026-02-26 09:05:30 -0300

    :sparkles: feat(modal): introduce onRequestClose support for native back button handling

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -11,12 +11,18 @@ export function App() {
       <View style={styles.container}>
         <Button onPress={() => setVisible(true)}>Abrir Modal</Button>
 
+        {/*
+          Quando o usuário está utilizando os recursos do SO, ele pode fechar o
+          modal, no caso do page/modal sheet, clicando no botão de voltar do
+          celular ou arrastando para baixo. Para detectar esse evento e alterar
+          o estado do modal, utilizamos a prop onRequestClose.
+        */}
         <Modal
           visible={visible}
           animationType="slide"
           statusBarTranslucent
-          // transparent
           presentationStyle="pageSheet" // * iOS only
+          onRequestClose={() => setVisible(false)}
         >
           <View style={styles.modalOverlay}>
             <View style={styles.modalContainer}>

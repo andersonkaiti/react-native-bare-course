47fecca8e198b2a49f5d5702e4407cdcc3ca4275
andersonkaiti <anderkaiti@gmail.com>
2026-02-28 22:38:10 -0300

    :sparkles: feat(image): introduce remote images into the Image component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,4 @@
 import { Image, SafeAreaView, View } from 'react-native'
-import gtr from '../images/gtr.jpg'
 import { styles } from './styles'
 
 export function App() {
@@ -7,22 +6,42 @@ export function App() {
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         {/*
-          Adicionar uma imagem é igual ao React na web: importamos a imagem e a
-          passamos como valor da prop source.
+          Para utilizar uma imagem remota, passamos um objeto para a prop source
+          com a propriedade uri.
 
-          Detalhe: o valor do import é um número que representa a imagem.
+          O uri não necessariamente é uma URL, então é possível inserir uma
+          base64 e a imagem será renderizada.
 
-          Também é possível fazer um require dentro da prop source, como:
-          <Image source={require('../images/gtr.jpg')} />
+          Caso não haja uma dimensão definida para a imagem, na web:
+          1. Ela será renderizada com 0x0px, que não ocupa espaço na tela.
+          2. O download da imagem é realizado.
+          3. O elemento é atualizado para ter as dimensões da imagem baixada.
+
+          No React Native, ela para na primeira etapa:
+          1. Ela será renderizada com 0x0px, que não ocupa espaço na tela.
+
+          No entanto, isso é intencional para evitar os pulos de layout que
+          acontecem quando a imagem é baixada.
+
+          Importante: isso só acontece com imagens remotas.
         */}
         <Image
-          source={gtr}
-          // Texto alternativo para leitores de tela.
-          alt="GTR na estrada"
-          style={{
+          source={{
+            uri: 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            // É possível definir as dimensões da imagem diretamente no objeto
+            // passado para a prop source.
             width: 300,
             height: 150,
+            // O body, o headers e o method podem ser enviados, pois ao utilizar
+            // a URL, estamos fazendo uma requisição HTTP para algum servidor que
+            // armazena a imagem.
+            method: 'POST',
+            body: JSON.stringify({ name: 'Anderson' }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
           }}
+          alt="GTR na estrada"
         />
       </View>
     </SafeAreaView>

794cc7c5e8198b05235f0f6780d45e8ecf7057bd
andersonkaiti <anderkaiti@gmail.com>
2026-03-01 22:14:40 -0300

    :sparkles: feat(image): introduce defaultSource prop to display fallback during loading

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -9,33 +9,27 @@ export function App() {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        {/*
-          No Android, existe um bug ao utilizar resizeMode="contain" e
-          borderRadius juntos. Para contornar, é necessário adicionar a
-          prop overlayColor, ou simplesmente criar um container wrapper.
-        */}
-        <View
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          }}
           style={{
             width: 300,
             height: 150,
-            borderRadius: 20,
-            overflow: 'hidden',
           }}
-        >
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            }}
-            style={{
-              width: 300,
-              height: 150,
-              backgroundColor: 'purple',
-            }}
-            alt="GTR na estrada"
-            resizeMode="contain"
-            blurRadius={isBlurred ? 50 : 0}
-          />
-        </View>
+          alt="GTR na estrada"
+          resizeMode="contain"
+          blurRadius={isBlurred ? 50 : 0}
+          /**
+            A prop defaultSource permite definir qual imagem deve ser
+            utilizada enquanto o carregamento da imagem principal não
+            for concluído.
+
+            Ele também aceita uma URI para imagens externas, mas no iOS ele
+            só aceita arquivos locais ou em base64.
+           */
+          defaultSource={require('../images/image-placeholder.png')}
+        />
 
         <Button onPress={() => setIsBlurred(!isBlurred)}>
           {isBlurred ? 'Mostrar imagem' : 'Borrar imagem'}

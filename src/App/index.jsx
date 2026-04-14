252a0e32c2ec221ff02b003f499f238d50773064
andersonkaiti <anderkaiti@gmail.com>
2026-04-14 01:12:06 -0300

    :sparkles: feat(pixel-ratio): introduce image pixel ratio nomenclature

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,11 +1,31 @@
 import {
+  Image,
   PixelRatio,
   SafeAreaView,
   Text,
   useWindowDimensions,
   View,
 } from 'react-native'
+import userIcon from '../images/user-icon/user-icon.png'
 import { styles } from './styles'
+
+/**
+  Dependendo do tamanho de uma imagem, em um pixel ratio maior ela poderá
+  perder a qualidade caso. Para solucionar isso, é preciso utilizar vetores ou,
+  é preciso usar imagens com diferentes resoluções, dependendo do pixel ratio
+  do dispositivo, como:
+  - user-icon.png
+  - user-icon@2x.png
+  - user-icon@3x.png
+
+  Convenção de nomenclatura de imagens:
+  user-icon{{suffix}}.png -> indica que a imagem tem o dobro ou o triplo
+  do tamanho original.
+  @ -> at, que significa "no/em", ou seja, a imagem está "no" formato 3x.
+
+  Caso haja variáveis variações da imagem com @2x, @3x, etc., o React Native
+  escolherá a imagem com a resolução mais próxima do pixel ratio do dispositivo.
+*/
 
 export function App() {
   const { fontScale, scale, width, height } = useWindowDimensions()
@@ -25,42 +45,21 @@ export function App() {
 
         <Text>Orientation: {orientation}</Text>
 
-        {/*
-          O PixelRatio é uma classe que fornece informações sobre a densidade
-          de pixels do dispositivo, mas a sua implementação no código-fonte do
-          React Native utiliza a Dimensions API.
-
-          A única diferença entre o PixelRatio.get() e o useWindowDimensions()
-          é que apenas o useWindowDimensions() permite monitorar mudanças na
-          tela, como rotação e zoom.
-        */}
         <Text>{PixelRatio.get()}</Text>
 
         <Text>{PixelRatio.getFontScale()}</Text>
 
-        {/*
-          Pixel Size = Physical Pixel
-          Layout Size = Logical Pixel
-
-          O método getPixelSizeForLayoutSize() é usado para converter um
-          tamanho de layout em pixels físicos.
-        */}
         <Text>{PixelRatio.getPixelSizeForLayoutSize(100)}</Text>
 
-        {/*
-          O startDetecting() é uma função no-op (no operation), ou seja, que
-          não faz nada.
-        */}
-        {/* <Text>{PixelRatio.startDetecting()}</Text> */}
-
-        {/* 
-          O roundToNearestPixel() é usado para arredondar um tamanho de layout
-          para o pixel físico mais próximo.
-
-          Ele multiplica o valor pelo pixel ratio, arredonda para cima ou para
-          baixo dependendo do valor e divide pelo pixel ratio.
-        */}
         <Text>{PixelRatio.roundToNearestPixel(8.44)}</Text>
+
+        <Image
+          source={userIcon}
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        />
       </View>
     </SafeAreaView>
   )

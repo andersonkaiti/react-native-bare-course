f8ca54604bba2ef71c9ba895d3119d228f966196
andersonkaiti <anderkaiti@gmail.com>
2026-04-16 14:36:15 -0300

    :sparkles: feat(input-accessory-view): introduce InputAccessoryView component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,64 +1,31 @@
-import { useRef } from 'react'
-import { DrawerLayoutAndroid, SafeAreaView, Text, View } from 'react-native'
-import { Button } from '../components/Button'
+import { InputAccessoryView, SafeAreaView, Text, View } from 'react-native'
+import { Input } from '../components/Input'
 import { styles } from './styles'
 
-/**
-  DrawerLayoutAndroid
-  - Menu lateral.
-  - O componente nativo só existe para Android.
-
-  Para abrir o menu, é necessário fazer isso imperativamente, ou seja:
-  manipulando drawer a partir de uma ref e chamando o método openDrawer().
-
-  Além disso, ele precisa ser um wrapper de tudo na tela.
-*/
 export function App() {
-  const drawerRef = useRef()
-
-  function handleOpenDrawer() {
-    drawerRef.current?.openDrawer()
-  }
-
-  function handleCloseDrawer() {
-    drawerRef.current?.closeDrawer()
-  }
-
   return (
-    <DrawerLayoutAndroid
-      ref={drawerRef}
-      // Define a posição do drawer
-      drawerPosition="left"
-      // Define o conteúdo do drawer
-      drawerWidth={250}
-      // Define como o drawer pode ser aberto
-      // locked-closed: trava e impede a abertura por gestos, mas ainda é
-      // possível clicar no overlay para fechar
-      // locked-open: trava todos os gestos e a abertura e fechamento deve ser
-      // tratada imperativamente
-      // unlocked: permite a abertura por gestos
-      drawerLockMode="locked-open"
-      // drawerBackgroundColor="#000000"
-      renderNavigationView={() => (
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#000',
-          }}
-        >
-          <Text style={{ color: '#fff' }}>Hello, Drawer!</Text>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Input placeholder="Nome" inputAccessoryViewID="name" />
+        <Input placeholder="E-mail" inputAccessoryViewID="email" />
+        <Input placeholder="Idade" inputAccessoryViewID="age" />
+      </View>
 
-          <Button onPress={handleCloseDrawer}>Fechar menu</Button>
-        </View>
-      )}
-    >
-      <SafeAreaView style={styles.wrapper}>
-        <View style={styles.container}>
-          <Button onPress={handleOpenDrawer}>Abrir menu</Button>
-        </View>
-      </SafeAreaView>
-    </DrawerLayoutAndroid>
+      {/*
+        O InputAccessoryView (* iOS only) é uma view que aparece acima do
+        teclado.
+      */}
+      <InputAccessoryView nativeID="name">
+        <Text>Accessory do nome</Text>
+      </InputAccessoryView>
+
+      <InputAccessoryView nativeID="email">
+        <Text>Accessory do email</Text>
+      </InputAccessoryView>
+
+      <InputAccessoryView nativeID="age">
+        <Text>Accessory da idade</Text>
+      </InputAccessoryView>
+    </SafeAreaView>
   )
 }
